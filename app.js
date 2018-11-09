504d034580cd9984320d3620976017bc6786e00b
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-08 17:22:06 -0800

    quotes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 
 const jsx = <p>test</p>;
 
-ReactDom.render(jsx, document.getElementById("app"));
+ReactDom.render(jsx, document.getElementById('app'));
